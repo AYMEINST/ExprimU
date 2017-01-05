60e53b8188310908a621ffc659efde40971f02d1
mndiaye27 <aymeinstitut@gmail.com>
2017-01-05 13:38:50 +0100

    first commit  for  initialisation

---
+++ src/main/java/org/com/fr/ExprimUApplication.java
@@ -0,0 +1,12 @@
+package org.com.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExprimUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExprimUApplication.class, args);
+	}
+}
