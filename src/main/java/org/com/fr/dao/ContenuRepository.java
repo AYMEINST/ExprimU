3f36366ed511c93be7a816b7fbe06fd9a3d0d20f
mndiaye27 <aymeinstitut@gmail.com>
2017-01-09 15:25:37 +0100

    deleting  Contenue  entities

--- src/main/java/org/com/fr/dao/ContenuRepository.java
+++
@@ -1,5 +0,0 @@
-package org.com.fr.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.com.fr.entity.Contenu;
-public interface ContenuRepository extends JpaRepository<Contenu, Long> {
-}
