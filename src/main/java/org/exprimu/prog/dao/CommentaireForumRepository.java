93ccec887a89e4cb4d7d1b5c3d0e3b010a75bc4c
mndiaye27 <aymeinstitut@gmail.com>
2017-01-18 10:44:15 +0100

    Khaled  modification  on  Forum  and  Forumcommentaire  entities

--- src/main/java/org/exprimu/prog/dao/CommentaireForumRepository.java
+++
@@ -1,5 +0,0 @@
-package org.exprimu.prog.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.exprimu.prog.entity.CommentaireForum;
-public interface CommentaireForumRepository extends JpaRepository<CommentaireForum, Long> {
-}
--- src/main/java/org/exprimu/prog/dao/ForumRepository.java
+++
@@ -1,5 +0,0 @@
-package org.exprimu.prog.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.exprimu.prog.entity.Forum;
-public interface ForumRepository extends JpaRepository<Forum, Long> {
-}
